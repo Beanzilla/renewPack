ee2c4cb0b3a1461cb00d5d8879df1898537542f9
david <beanzilla@red-green.com>
2021-09-08 16:47:07 -0400

    Reduced Resource Costs to support more MCL
      It was found that in MCL it was practically impossible to make
    renewing anything as it costs a large amount of leaves. (This fix
    reverts to the original way we do things)

--- renew_plant/init.lua
+++ renew_plant/init.lua
@@ -11,37 +11,12 @@ minetest.register_craftitem("renew_plant:plant", {
     inventory_image = "renew_plant_plant1.png"
 })
 
-minetest.register_craftitem("renew_plant:plant_duo", {
-    description = renew_plant.S("Renewable Plant Mend"),
-    inventory_image = "renew_plant_plant2.png"
-})
-
-minetest.register_craftitem("renew_plant:plant_tri", {
-    description = renew_plant.S("Renewable Plant Bond"),
-    inventory_image = "renew_plant_plant3.png"
-})
-
 minetest.register_craft({
     type = "fuel",
     recipe = "renew_plant:plant",
     burntime = 40 -- 10 x https://github.com/minetest/minetest_game/blob/master/mods/default/crafting.lua#L655
 })
-
-minetest.register_craft({
-    type = "fuel",
-    recipe = "renew_plant:plant_duo",
-    burntime = 75
-})
-
-minetest.register_craft({
-    type = "fuel",
-    recipe = "renew_plant:plant_tri",
-    burntime = 145
-})
-
 local p1 = "renew_plant:plant"
-local p2 = "renew_plant:plant_duo"
-local p3 = "renew_plant:plant_tri"
 
 minetest.register_craft({
     output = p1.." 1",
@@ -53,43 +28,9 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-    output = p2.." 1",
-    recipe = {
-        {p1, p1, p1},
-        {p1, p1, p1},
-        {p1, p1, p1}
-    }
-})
-
-minetest.register_craft({
-    output = p3.." 1",
-    recipe = {
-        {p2, p2, p2},
-        {p2, p2, p2},
-        {p2, p2, p2}
-    }
-})
-
-minetest.register_craft({
     type = "shapeless",
     output = "default:leaves 9",
     recipe = {
         p1
-    }
-})
-
-minetest.register_craft({
-    type = "shapeless",
-    output = p1.." 9",
-    recipe = {
-        p2
-    }
-})
-
-minetest.register_craft({
-    type = "shapeless",
-    output = p2.." 9",
-    recipe = {
-        p3
     }
 })
